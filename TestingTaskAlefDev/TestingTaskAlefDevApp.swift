ab8e8fbc2be91cc5e84143b2ab330a342a746d2d
MIzvarin <86963802+MIzvarin@users.noreply.github.com>
2021-11-08 14:05:23 +0300

    Initial Commit

---
+++ TestingTaskAlefDev/TestingTaskAlefDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTaskAlefDevApp.swift
+//  TestingTaskAlefDev
+//
+//  Created by Максим Изварин on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestingTaskAlefDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
